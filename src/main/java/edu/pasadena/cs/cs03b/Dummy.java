b0ce57d845564f1765dd03acba191383e6bdcc44
Yesenia Garcia <yesgarcia583@student.citruscollege.edu>
2025-11-29 22:00:18 +0000

    add logic

--- src/main/java/edu/pasadena/cs/cs03b/Dummy.java
+++ src/main/java/edu/pasadena/cs/cs03b/Dummy.java
@@ -1,9 +1,14 @@
 package edu.pasadena.cs.cs03b;
 
+import java.util.Scanner
+
 public class Dummy {
 
 	public static void main(String[] args) {
-		// TODO: add you logic here
+	public static double getTaxRate(double annualIncome)
+
+	    int[] salaryTable = {20000, 5000, 100000, Integer.Max_Value};
+		double[] taxRateTable = {0.10,0.20, 0.30, 0.40};
 	}
 
 	public static int dummy() {
